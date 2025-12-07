80d3a86908d1ec1b77f11d496b09114796259b12
puneet1983 <puneetpuneet2009@gmail.com>
2025-12-07 14:54:53 +0000

    first commit

---
+++ tourism_project/hosting/hosting.py
@@ -0,0 +1,19 @@
+from huggingface_hub import HfApi
+import os
+import subprocess
+from google.colab import userdata
+
+user_id = "puneet83"
+
+HF_TOKEN = userdata.get("HF_TOKEN")
+
+if HF_TOKEN is None:
+    raise ValueError("❌ HF_TOKEN not added. Please set it using userdata.set().")
+
+api = HfApi(token=userdata.get('HF_TOKEN'))
+api.upload_folder(
+    folder_path="tourism_project/deployment",     # the local folder containing your files
+    repo_id=f"{user_id}/tourism_product",          # the target repo
+    repo_type="space",                      # dataset, model, or space
+    path_in_repo="",                          # optional: subfolder path inside the repo
+)
